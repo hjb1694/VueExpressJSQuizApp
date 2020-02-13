4133d3f34e7262fc82db9449e2d4dcf3ca6a1c2d
Hayden Bradfield <hbradfield20@gmail.com>
2020-02-13 02:41:46 -0500

    created a separate component for results

---
+++ client/src/components/Results.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Your Results</h1>
+    <div class="mt-4">
+      <div
+        class="px-4 py-4 mt-4 elevation-6"
+        v-for="result of results"
+        :key="result.questionContent"
+      >
+        <h3>{{result.questionContent}}</h3>
+        <p class="mt-4">
+          <strong>Result:</strong>
+          {{result.correctIncorrect}}
+        </p>
+        <p>
+          <strong>Correct Answer:</strong>
+          {{result.correctAnswerContent}}
+        </p>
+        <p>
+          <strong>Your Answer:</strong>
+          {{result.userAnswerContent}}
+        </p>
+      </div>
+    </div>
+    <v-btn
+      class="mt-4"
+      @click="$router.push({name : 'quizList'})"
+      text
+      color="indigo"
+    >Back to Quizzes</v-btn>
+  </div>
+</template>
+<script>
+export default {
+  props: ["results"]
+};
+</script>
--- client/src/pages/Quiz.vue
+++ client/src/pages/Quiz.vue
@@ -3,34 +3,7 @@
     <v-container>
       <div style="margin-top:3.5rem;">
         <div v-if="showResults">
-          <h1>Your Results</h1>
-          <div class="mt-4">
-            <div
-              class="px-4 py-4 mt-4 elevation-6"
-              v-for="result of results"
-              :key="result.questionContent"
-            >
-              <h3>{{result.questionContent}}</h3>
-              <p class="mt-4">
-                <strong>Result:</strong>
-                {{result.correctIncorrect}}
-              </p>
-              <p>
-                <strong>Correct Answer:</strong>
-                {{result.correctAnswerContent}}
-              </p>
-              <p>
-                <strong>Your Answer:</strong>
-                {{result.userAnswerContent}}
-              </p>
-            </div>
-          </div>
-          <v-btn
-            class="mt-4"
-            @click="$router.push({name : 'quizList'})"
-            text
-            color="indigo"
-          >Back to Quizzes</v-btn>
+          <results :results="results" />
         </div>
         <div v-else>
           <template v-if="quizItems">
@@ -62,6 +35,7 @@
 </template>
 <script>
 import quizService from "../services/quizService";
+import Results from "../components/Results";
 
 export default {
   data() {
@@ -73,6 +47,9 @@ export default {
       showResults: false,
       results: []
     };
+  },
+  components: {
+    results: Results
   },
   methods: {
     async fetchQuiz() {
