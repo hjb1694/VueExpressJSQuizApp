80fb7e75a7636e234187208e2c8fe49c7db4d76a
Hayden Bradfield <hbradfield20@gmail.com>
2020-02-14 02:07:48 -0500

    some additions to the home component

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -14,6 +14,47 @@
         </v-col>
       </v-row>
     </v-parallax>
-    <div style="height:500px;"></div>
+    <v-container>
+      <div style="min-height:500px;margin:2rem 0;" class="home-section-1">
+        <div class="home-img elevation-6"></div>
+        <div class="briefing">
+          <h2 class="briefing__heading">A Free, Blazing Fast, Simple Quiz Platform</h2>
+          <p class="briefing__text">
+            Build up your JavaScript skills utilizing a user-friendly quiz platform. View your results instantly!
+            Did we mention it is free? There is absolutely no cost to you!
+          </p>
+        </div>
+      </div>
+    </v-container>
   </div>
 </template>
+<style scoped>
+.home-img {
+  width: 100%;
+  height: 20rem;
+  background-image: url(../assets/js_quiz_ss.png);
+  background-position: center;
+  background-size: cover;
+}
+
+.briefing {
+  padding: 1rem 2rem;
+}
+
+.briefing__heading {
+  color: #303f9f;
+  margin-top: 1rem;
+}
+
+.briefing__text {
+  font-size: 1.2rem;
+  margin-top: 1.2rem;
+}
+
+@media (min-width: 750px) {
+  .home-section-1 {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+</style>
